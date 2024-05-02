b7268ba83c3ea466262962151f5a9eb00e3eea07
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2024-05-02 14:33:07 +0800

    Update JSON

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: 5173,
+    port: 3000,
     strictPort: true,
   },
   server: {
-    port: 5173,
+    port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:5173",
+    origin: "http://0.0.0.0:3000",
   },
 });
